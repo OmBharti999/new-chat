c8caeec54fcb493f98254b14681399fad485361b
om_official <ombharti888@gmail.com>
2024-07-26 03:05:32 +0530

    udpate login form

--- components/shared/LoginForm.tsx
+++ components/shared/LoginForm.tsx
@@ -99,7 +99,10 @@ export function SigninForm() {
       <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
         Welcome to Chat App by Om
       </h2>
-
+      <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+        Login to Chat App by Om if you can because we don&apos;t have a login
+        flow yet
+      </p>
       <form className="my-8" onSubmit={handleSubmit}>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="email">Email Address</Label>
@@ -146,5 +149,12 @@ const LabelInputContainer = ({
 
 export const LoginForm = () => {
   const [isSigningIn, setIsSigningIn] = useState(true);
-  return <div>{isSigningIn ? <SigninForm /> : <SignupForm />}</div>;
+  return (
+    <div>
+      <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black mb-4">
+        <button onClick={() => setIsSigningIn((p) => !p)}>Sign in</button>
+      </div>
+      {isSigningIn ? <SigninForm /> : <SignupForm />}
+    </div>
+  );
 };
